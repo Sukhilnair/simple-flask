3d65d8d08cb0f5760d30db71859d389813759cc7
SukhilNairEEN <sukhil@een.com>
2024-07-20 18:57:11 +0530

    Added script to run automation.

---
+++ shell-to-run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+sudo apt-get update -y
+sudo apt-get install python3 -y
+sudp apt-get install python3-pip -y
+python3 -m pip install flask
+sudo apt-get install nginx -y
+echo 'server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    root /var/www/html;
+    index index.html index.htm index.nginx-debian.html;
+    server_name _;
+
+    location / {
+        proxy_pass http://127.0.0.1:8080;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+    }
+}' | sudo tee /etc/nginx/sites-enabled/default >/dev/null
+
+sudo systemctl restart nginx 
+python3 sukhil-fla.py &
